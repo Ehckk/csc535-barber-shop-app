58604fce667897031205b4d21285cdbd7547c8dc
Eric <throwawayehcksdee@gmail.com>
2024-02-24 13:51:55 -0500

    db connector

--- config/__init__.py
+++ config/__init__.py
@@ -4,18 +4,17 @@ from pathlib import Path
 
 CWD = Path.cwd()
 
-def load_config(filename=".flaskenv"):
-    config_path = Path.joinpath(CWD, "config", filename)
+def load(filename=".flaskenv"):
+    parent = Path(__file__).parent
+    config_path = Path.joinpath(CWD, parent, filename)
     print(f"Loading configuration from {config_path}...")
+
     if not Path.exists(config_path):
         raise FileNotFoundError(
-            "\".flaskenv\" file not found. Did you forget to create it? Is it in the \"config\" folder?"
+            "\'.flaskenv\' file not found. Did you forget to create it? Is it in the \'config\' folder?"
         )
+    
     print("Configuration loaded.")
     return dotenv_values(config_path)
 
-
-app_config = load_config()
-
-if __name__ == "__main__":
-    print(app_config)
+app_config = load()
