5ae9615a02994d2b88113f483128e084cec079fc
Eric <throwawayehcksdee@gmail.com>
2024-02-24 13:51:55 -0500

    db connector

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from config import app_config
+from data.connect import connect_db
 
 
 def create_app():
     print("Starting application...")
-    app = Flask(__name__)
     
+    app = Flask(__name__)
     app.secret_key = "dev"
     app.config.from_mapping(app_config)
+
+
+    connect_db(app_config)
 
     @app.route("/")
     def test():
---
+++ data/connect.py
@@ -0,0 +1,23 @@
+from pymysql import Connection
+from pymysql.cursors import DictCursor 
+
+
+def connect_db(config):
+    try:
+        print("Connecting to database...")
+        connection = Connection(
+            host=config["MYSQL_IP"],
+            port=int(config["MYSQL_PORT"]),
+            user=config["MYSQL_USER"],
+            password=config["MYSQL_PASSWORD"],
+            database=config["MYSQL_DB"],
+            charset='utf8mb4',
+            cursorclass=DictCursor
+        )
+    except KeyError as key:
+        message = f"Unable to find required environment variable {key} in \'.flaskenv\'. Does it exist?"
+        raise KeyError(message)
+    
+    name = connection.db.decode("ascii")
+    print(f"Connected to database \'{name}\'")
+    return connection
