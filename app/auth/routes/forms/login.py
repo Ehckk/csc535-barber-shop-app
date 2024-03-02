f6167ab33dcaaf2abb3323c6e760db824500a0b7
Eric <throwawayehcksdee@gmail.com>
2024-03-02 09:37:15 -0500

    login form

--- app/auth/routes/forms/login.py
+++ app/auth/routes/forms/login.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField("username", validators=[DataRequired()])
+    email = StringField("email", validators=[DataRequired()])
     password = PasswordField("password", validators=[DataRequired()])
