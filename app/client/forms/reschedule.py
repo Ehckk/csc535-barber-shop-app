9b996bc6821ed10d610ae21c8441a434389ba137
mamood98 <aser.mame97@gmail.com>
2024-03-23 11:21:52 -0400

    Forms and routes

---
+++ app/client/forms/reschedule.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import  DateField, TimeField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
+
+class RescheduleForm(FlaskForm):
+    new_date = DateField('New Date', validators=[DataRequired()], format='%Y-%m-%d')
+    new_time = TimeField('New Time', validators=[DataRequired()], format='%H:%M')
+    new_duration = IntegerField('New Duration', validators=[DataRequired(), NumberRange(min=1)])
