f1dde600a5dc234a08996bb80e7b697b0872d539
Eric <throwawayehcksdee@gmail.com>
2024-03-31 09:31:01 -0400

    cards

--- app/barber/routes/__init__.py
+++ app/barber/routes/__init__.py
@@ -1,7 +1,6 @@
 __all__ = [
     "home",
     "calendar",
-    "appointments",
     "view_appointment",
     "cancel_appointment"
 ]
--- app/barber/routes/home.py
+++ app/barber/routes/home.py
@@ -1,4 +1,6 @@
 from flask import render_template
+
+from ...queries.appointments import list_barber_appointments
 from ...utils.user import current_user
 from .. import barber
 
@@ -6,4 +8,10 @@ from .. import barber
 @barber.route("/", methods=["GET", "POST"])
 def barber_home():
     user = current_user()
-    return render_template('barber/home.html', user=user)
+    appointments = list_barber_appointments(user.id)
+    print(appointments)
+    return render_template(
+        'barber/home.html', 
+        user=user,
+        appointments=appointments
+    )
