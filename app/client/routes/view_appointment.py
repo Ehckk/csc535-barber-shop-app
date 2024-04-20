dfdeda2ecf7cc93809fd95e4b3a606d7ca59bc1f
Eric <throwawayehcksdee@gmail.com>
2024-04-20 15:18:20 -0400

    services on appointment details - client

--- app/client/routes/view_appointment.py
+++ app/client/routes/view_appointment.py
@@ -1,7 +1,8 @@
 from flask import render_template
 
 from ...utils.decorators import has_role
-from ...queries import appointments 
+from ...utils.table import get_services_table
+from ...queries import appointments, services
 from ...utils.user import current_user
 from .. import client
 
@@ -12,7 +13,10 @@ def appointment_details(appt_id):
     user = current_user()
     client_appointments = appointments.list_client_appointments(user.id)
     requested_appointments = appointments.list_client_appointments(user.id, is_booked=False)
+
     appointment = appointments.retrieve_appointment(appt_id)
+    appointment_services = services.retrieve_appointment_services(appt_id)
+    services_data = get_services_table(appointment_services)
 
     return render_template(
         "client/view_appointment.html", 
@@ -20,5 +24,6 @@ def appointment_details(appt_id):
         appointments=client_appointments,
         requested_appointments=requested_appointments,
         appointment=appointment,
-        appt_id=appt_id
+        appt_id=appt_id,
+        services_data=services_data
     )
