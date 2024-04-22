699a9270483272a3ebd3b03a9ae363e565e52221
Ian Cheung <ij4.cheung@gmail.com>
2024-04-22 16:24:05 -0400

    client reschedule appointment

--- app/barber/routes/forms/reschedule.py
+++ app/barber/routes/forms/reschedule.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import  DateField, TimeField, IntegerField
+from wtforms import  DateField, TimeField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
 class RescheduleForm(FlaskForm):
     new_date = DateField('New Date', validators=[DataRequired()], format='%Y-%m-%d')
     new_time = TimeField('New Time', validators=[DataRequired()], format='%H:%M')
     new_duration = IntegerField('New Duration', validators=[DataRequired(), NumberRange(min=1)])
+    submit = SubmitField("Reschedule Appointment")
