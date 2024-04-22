fbbe22992c8190994a1a680e6102d74b080538fd
root <root@Thinkpad-JH>
2024-04-22 16:33:47 -0400

    history

--- app/barber/routes/appointment_history.py
+++ app/barber/routes/appointment_history.py
@@ -2,11 +2,19 @@ from flask import render_template
 from ...utils.decorators import has_role
 from ...models.appointment import Appointment
 from ...utils.user import current_user
-from ...queries.appointments import list_barber_appointments
+from ...queries.appointments import list_barber_history
 from .. import barber
+
 
 
 @barber.route("/history", methods=["GET"])
 @has_role("Barber")
 def appointment_history():
-    return "Barber Appointment History"
+    user = current_user()
+    historical_appointments = list_barber_history(user.id) 
+
+    return render_template(
+        'barber/appointment_history.html',
+        user=user,
+        historical_appointments=historical_appointments
+    )
--- app/client/routes/appointment_history.py
+++ app/client/routes/appointment_history.py
@@ -1,12 +1,19 @@
 from flask import render_template
-
 from ...utils.decorators import has_role
+from ...models.appointment import Appointment
 from ...utils.user import current_user
-from ...queries.appointments import list_client_appointments
-from .. import client
+from ...queries.appointments import list_client_history
+from .. import  client
 
 
 @client.route("/history", methods=["GET"])
 @has_role("Client")
-def appointment_history():
-    return "client Appointment History"
+def client_appointment_history():
+    user = current_user()
+    historical_appointments = list_client_history(user.id) 
+
+    return render_template(
+        'client/appointment_history.html',
+        user=user,
+        historical_appointments=historical_appointments
+    )
