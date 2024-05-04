18f9dfe69db249bad4e61b71f025130a68390bd0
Eric <throwawayehcksdee@gmail.com>
2024-05-04 10:21:09 -0400

    cancel appointment email notifs

--- app/barber/routes/cancel_appointment.py
+++ app/barber/routes/cancel_appointment.py
@@ -1,10 +1,29 @@
 from flask import flash, redirect, url_for
-from ...queries.appointments import delete_appointment
+from ...utils.decorators import has_role
+from ...utils.email import send_mail
+from ...queries.appointments import delete_appointment, retrieve_appointment
 from .. import barber
 
 
 @barber.route("/cancel/<int:appt_id>", methods=["POST", "GET"])
+@has_role("Barber")
 def cancel_appointment(appt_id):
+    appointment = retrieve_appointment(appt_id)
     delete_appointment(appt_id)  
-    flash('Appointment Canceled!', category="success")
+
+    message = """
+        An appointment has been canceled.
+
+        Appointment: {appt}
+    """.format(appt=str(appointment))
+    send_mail(
+        subject="Appointment Canceled",
+        recipients=[
+            appointment.barber.email,
+            appointment.client.email
+        ],
+        body=message
+    )
+
+    flash("Appointment Canceled!", category="success")
     return redirect(url_for('barber.barber_home'))
